91610e19ba59e69cc317612ab828b24c000ff09e
Nipunan Rangana <100415127+Nipuna-apps@users.noreply.github.com>
2022-10-04 19:09:18 +0530

    📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶

--- settings.js
+++ settings.js
@@ -54,15 +54,15 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['94714188730'] //ur owner number
+global.vcardowner = ['94706540311'] //ur owner number
 global.ownername = "nipuna rangana" //ur owner name
 global.ytname = "YT: 𝐂𝐈𝐁𝐄𝐑 𝐓𝐄𝐂𝐇" //ur yt chanel name
 global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
-//bot bomdy 
-global.owner = ['94714188730']
-global.ownertag = '94714188730' //ur tag number
+//bot bomdy
+global.owner = ['94706540311']
+global.ownertag = '94706540311' //ur tag number
 global.botname = '📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶' //ur bot name
 global.linkz = "https://youtu.be/3J9-8U8St3U" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/3J9-8U8St3U" //ur website to be displayed
